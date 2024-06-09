978b0405fc73209836558ae2e046185109a08d7b
JohN100x1 <JohN100x1@hotmail.co.uk>
2024-06-09 15:57:55 +0100

    feat: complete week 4

---
+++ src/week_4.py
@@ -0,0 +1,95 @@
+from math import inf, prod
+from random import randint, choices
+
+from week_3 import motif_laplace_profile, profile_probable_kmer, motif_score
+
+
+def profile_probable_motifs(
+    dna: list[str], profile: list[list[float]]
+) -> list[str]:
+    """Returns the profile-most probable motifs from dna."""
+    k = len(profile[0])
+    return [profile_probable_kmer(text, k, profile) for text in dna]
+
+
+def randomised_motif_search(dna: list[str], k: int, t: int) -> list[str]:
+    """Return the best motifs from a random starting motifs."""
+    motifs = []
+    for string in dna[:t]:
+        start_index = randint(0, len(string) - k)
+        motifs.append(string[start_index : start_index + k])
+    best_motifs = motifs
+    best_score = motif_score(motifs)
+    while True:
+        profile = motif_laplace_profile(motifs)
+        motifs = profile_probable_motifs(dna, profile)
+        score = motif_score(motifs)
+        if score < best_score:
+            best_motifs = motifs
+            best_score = score
+        else:
+            return best_motifs
+
+
+def repeated_random_motif_search(
+    dna: list[str], k: int, t: int, repeats: int
+) -> list[str]:
+    """Return the best motifs by score using repeating random motif search."""
+    best_motifs = []
+    best_score = inf
+    for _ in range(repeats):
+        motifs = randomised_motif_search(dna, k, t)
+        score = motif_score(motifs)
+        if score < best_score:
+            best_motifs = motifs
+            best_score = score
+    return best_motifs
+
+
+def gibbs_sampler(dna: list[str], k: int, t: int, n: int) -> list[str]:
+    """Return the best motifs using Gibbs sampling."""
+    idx_map = {"A": 0, "C": 1, "G": 2, "T": 3}
+    motifs = []
+    for string in dna[:t]:
+        start_index = randint(0, len(string) - k)
+        motifs.append(string[start_index : start_index + k])
+    best_motifs = motifs
+    best_score = motif_score(motifs)
+    for _ in range(n):
+        i = randint(0, t - 1)
+        profile = motif_laplace_profile(motifs[:i] + motifs[i + 1 :])
+        kmers = [dna[i][idx : idx + k] for idx in range(len(dna[0]) - k + 1)]
+        weights = [
+            prod(profile[idx_map[char]][j] for j, char in enumerate(kmer))
+            for kmer in kmers
+        ]
+        motif = choices(kmers, weights=weights)[0]
+        motifs = motifs[:i] + [motif] + motifs[i + 1 :]
+        score = motif_score(motifs)
+        if score < best_score:
+            best_motifs = motifs
+            best_score = score
+    return best_motifs
+
+
+def repeated_gibbs_sampler(
+    dna: list[str], k: int, t: int, n: int, repeats: int
+) -> list[str]:
+    """Return the best motifs using repeated Gibbs sampling."""
+    best_motifs = []
+    best_score = inf
+    for _ in range(repeats):
+        motifs = gibbs_sampler(dna, k, t, n)
+        score = motif_score(motifs)
+        if score < best_score:
+            best_motifs = motifs
+            best_score = score
+    return best_motifs
+
+
+if __name__ == "__main__":
+    dna = ["AAGCCAAA", "AATCCTGG", "GCTACTTG", "ATGTTTTG"]
+    motifs = ["CCA", "CCT", "CTT", "TTG"]
+    profile = motif_laplace_profile(motifs)
+    motifs = profile_probable_motifs(dna, profile)
+    print(" ".join(motifs))
---
+++ tests/test_week_4.py
@@ -0,0 +1,50 @@
+from random import seed
+
+from week_4 import repeated_random_motif_search, repeated_gibbs_sampler
+
+
+def test_repeated_random_motif_search() -> None:
+    seed(0)
+    motifs = repeated_random_motif_search(
+        [
+            "CGCCCCTCTCGGGGGTGTTCAGTAAACGGCCA",
+            "GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG",
+            "TAGTACCGAGACCGAAAGAAGTATACAGGCGT",
+            "TAGATCAAGTTTCAGGTGCACGTCGGTGAACC",
+            "AATCCACCAGCTCCACGTGCAATGTTGGCCTA",
+        ],
+        8,
+        5,
+        1000,
+    )
+    assert motifs == [
+        "TCTCGGGG",
+        "CCAAGGTG",
+        "TACAGGCG",
+        "TTCAGGTG",
+        "TCCACGTG",
+    ]
+
+
+def test_gibbs_sample() -> None:
+    seed(1)
+    motifs = repeated_gibbs_sampler(
+        [
+            "CGCCCCTCTCGGGGGTGTTCAGTAACCGGCCA",
+            "GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG",
+            "TAGTACCGAGACCGAAAGAAGTATACAGGCGT",
+            "TAGATCAAGTTTCAGGTGCACGTCGGTGAACC",
+            "AATCCACCAGCTCCACGTGCAATGTTGGCCTA",
+        ],
+        8,
+        5,
+        100,
+        20,
+    )
+    assert motifs == [
+        "TCTCGGGG",
+        "CCAAGGTG",
+        "TACAGGCG",
+        "TTCAGGTG",
+        "TCCACGTG",
+    ]
